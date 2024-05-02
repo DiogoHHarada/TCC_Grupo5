db94dc34d819a3e94b5305d099b3c0b56481cfe4
DiogoHHarada <diogo.harada@unesp.br>
2024-05-02 18:31:58 -0300

    Estaca 0

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Criando a 1ª rota do curso
-Route::get('/dashboard', function () {
-    return "Contato";
-});
