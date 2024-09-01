234c439811d7983906b051acf06265a9c2738038
DiogoHHarada <diogo.harada@unesp.br>
2024-09-01 12:13:08 -0300

    mudanças simples no nome de js e css e foi introduzido eles na view sobrenós

--- resources/views/SobreNos/index.blade.php
+++ resources/views/SobreNos/index.blade.php
@@ -1,86 +1,88 @@
 @extends('layout.site')
 @section('titulo','AtmoVision')
 @section('conteudo') 
+    <link rel="stylesheet" href="/css/SobreNos.css">
 
- <!-- Primeira Seção -->
- <div class="grid-layout">
-    <div class="retangulo11">
-        <h1 class="jump">Atm Vision Project</h1>
-        <p>
-            Uma torre metelógica construída com harduíno e com um site mostrando em tempo real os resultados obtidos pela torre.
-        </p>
-    </div>
-    <div class="retangulo22">
-        <img src="/img/img_project.jpg" alt="Project Photo">
-    </div>
-</div>
-
-<!-- Animação 1-->
-<div class="animation">
-    <div class="scroll-wrapper">
-        <div class="scroll-text">- atmos vision - atmos vision - atmos vision - atmos vision - atmos vision - atmos vision </div>
-    </div>
-</div>
-
-<!-- Segunda Seção -->
-<div class="grid-layout">
-    <!-- The Team -->
-    <div class="retangulo2">
-        <img src="/img/img_team.jpg" alt="Team Photo">
-    </div>
-    <div class="retangulo1 light">
-        <h1 class="jump">Atm Vizion Team</h1>
-        <p>
-            Uma torre metelógica construída com harduíno e com um site mostrando em tempo real os resultados obtidos pela torre.
-        </p>
-    </div>
-
-    <!-- Project Manager -->
-    <div class="retangulo1">
-        <h1 class="jump">Orientador Atm Vizion</h1>
-        <p>
-            Uma torre metelógica construída com harduíno e com um site mostrando em tempo real os resultados obtidos pela torre.
-        </p>
-    </div>
-    <div class="retangulo2">
-        <img src="images/team_folder/img_andre.jpg" alt="Team Photo">
-    </div>
-
-    <!-- UX/UI Designer -->
-    <div class="retangulo2">
-        <img src="images/team_folder/img_matheus.jpg" alt="Team Photo">
-    </div>
-    <div class="retangulo1 light">
-        <h1 class="jump">Product Owner</h1>
-        <p>
-            Uma torre metelógica construída com harduíno e com um site mostrando em tempo real os resultados obtidos pela torre.
-        </p>
-    </div>
-
-    <!-- Developer Manager -->
-    <div class="retangulo1">
-        <h1 class="jump">Designer Interface</h1>
-        <p>
-            Uma torre metelógica construída com harduíno e com um site mostrando em tempo real os resultados obtidos pela torre.
-        </p>
-    </div>
-    <div class="retangulo2">
-        <img src="images/team_folder/img_ellen.jpg" alt="Team Photo">
-    </div>
-
-
-    <!-- Animação 2-->
-    <div class="animation">
-        <div class="scroll-wrapper">
-            <div class="scroll-text">- atmos vizion - atmos vizion - atmos vizion - atmos vizion - atmos vizion - atmos vizion </div>
+    <!-- Primeira Seção -->
+    <div class="grid-layout">
+        <div class="retangulo11">
+            <h1 class="jump">Atm Vision Project</h1>
+            <p>
+                Uma torre metelógica construída com arduíno e com um site mostrando em tempo real os resultados obtidos pela torre.
+            </p>
+        </div>
+        <div class="retangulo22">
+            <img src="/img/img_project.jpg" alt="Project Photo">
         </div>
     </div>
-</div>
 
-<!-- Identificação -->
-<div class="identidade">
-    <div class="retangulo-identidade"></div>
-    <div class="retangulo-identidade"></div>
-    <div class="retangulo-identidade"></div>
-    <div class="retangulo-identidade"></div>        
-</div>
+    <!-- Animação 1-->
+    <div class="animation">
+        <div class="scroll-wrapper">
+            <div class="scroll-text">- atmos vision - atmos vision - atmos vision - atmos vision - atmos vision - atmos vision </div>
+        </div>
+    </div>
+
+    <!-- Segunda Seção -->
+    <div class="grid-layout">
+        <!-- The Team -->
+        <div class="retangulo2">
+            <img src="/img/img_team.jpg" alt="Team Photo">
+        </div>
+        <div class="retangulo1 light">
+            <h1 class="jump">Atm Vision Team</h1>
+            <p>
+                Uma torre metelógica construída com harduíno e com um site mostrando em tempo real os resultados obtidos pela torre.
+            </p>
+        </div>
+
+        <!-- Project Manager -->
+        <div class="retangulo1">
+            <h1 class="jump">Orientador Atm Vision</h1>
+            <p>
+                Uma torre metelógica construída com harduíno e com um site mostrando em tempo real os resultados obtidos pela torre.
+            </p>
+        </div>
+        <div class="retangulo2">
+            <img src="images/team_folder/img_andre.jpg" alt="Team Photo">
+        </div>
+
+        <!-- UX/UI Designer -->
+        <div class="retangulo2">
+            <img src="images/team_folder/img_matheus.jpg" alt="Team Photo">
+        </div>
+        <div class="retangulo1 light">
+            <h1 class="jump">Product Owner</h1>
+            <p>
+                Uma torre metelógica construída com harduíno e com um site mostrando em tempo real os resultados obtidos pela torre.
+            </p>
+        </div>
+
+        <!-- Developer Manager -->
+        <div class="retangulo1">
+            <h1 class="jump">Designer Interface</h1>
+            <p>
+                Uma torre metelógica construída com harduíno e com um site mostrando em tempo real os resultados obtidos pela torre.
+            </p>
+        </div>
+        <div class="retangulo2">
+            <img src="images/team_folder/img_ellen.jpg" alt="Team Photo">
+        </div>
+
+        <!-- Animação 2-->
+        <div class="animation">
+            <div class="scroll-wrapper">
+                <div class="scroll-text">- atmos vision - atmos vision - atmos vision - atmos vision - atmos vision - atmos vision </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Identificação -->
+    <div class="identidade">
+        <div class="retangulo-identidade"></div>
+        <div class="retangulo-identidade"></div>
+        <div class="retangulo-identidade"></div>
+        <div class="retangulo-identidade"></div>        
+    </div>
+    <script src="/js/SobreNos.js"></script>
+@endsection
--- resources/views/layout/_rodape.blade.php
+++ resources/views/layout/_rodape.blade.php
@@ -1,3 +1,2 @@
-    <script src="/js/Cabecalho"></script>
    </body>
 </html>
