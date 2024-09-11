fd68f68e075b7db0b6526c24d7962dd23a90f441
DiogoHHarada <163158841+DiogoHHarada@users.noreply.github.com>
2024-09-10 21:14:09 -0300

    entrada substituida pela home

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -4,14 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Usuario;
 
 class UsuarioSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        //
+    public function run() { // exemplo que insere um adm no banco
+        $dados=['nome'=>"Thiago",'email'=>"admin@email",'senha'=>bcrypt("123")];
+        Usuario::create($dados);
     }
 }
